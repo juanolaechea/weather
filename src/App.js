9929411e1d9a07c374307c6f15639a347bcbfc27
juanolaechea <42986554+juanolaechea@users.noreply.github.com>
2022-01-30 16:32:49 -0300

    form and api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [find, setFind]=useState ({
+    city: '',
+    country: ''
+});
+
+ const [query, setQuery] = useState(false)
+
+  const {city, country} =find;
+
+
+  useEffect (() => {
+    
+    const queryAPI = async () => {
+
+     if(query){
+      const apiKey='709f7ded882e2e79e26d745445be33b3';
+      const url =`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
+
+      const answer = await fetch(url);
+      const result = await answer.json();
+
+      console.log(result);
+     }
+    }
+
+    queryAPI();
+
+  },[query]);
+
+
   return (
     <Fragment>
       <Header
@@ -12,7 +43,11 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario  />
+              <Formulario 
+              find={find}
+             setFind={setFind}
+             setQuery={setQuery}
+              />
             </div>
             <div className="col m6 s12">
               2
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,17 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Formulario = () => {
+const Formulario = ({find, setFind,setQuery}) => {
+
+
+    const [error, setError]=useState(false);
+    const {city, country} = find;
+
+    const handleChange = e => {
+
+        setFind({
+            ...find,
+            [e.target.name] : e.target.value
+        });
+    }
+    // onSubmit validate
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //validate 
+        if(city.trim() ==='' || country.trim() ===''){
+            setError(true);
+            return;
+        }
+        setError(false);
+
+        // send to app
+        setQuery(true);
+
+    }
+
+
     return ( 
         
-        <form>
+        <form
+        onSubmit={handleSubmit}
+        >
+            {error ? <p className="red darken-4 error">All fields are required! </p> : null}
             <div className= "input-field col s12">
                 <input
                     type="text"
                     name="city"
                     id="city" 
+                    value={city}
+                    onChange={handleChange}
                  />
-                 <label>City:</label>
-
+                 <label htmlFor="city" >City:</label>
+            </div>
+            <div className= "input-field col s12">
+              
+              <select 
+              name="country"
+               id="country"
+               value={country}
+               onChange={handleChange}
+               > 
+                    <option value="">--Select--</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+             </select>
+              
+                <label htmlFor="country">Country:</label>
+        
+            </div>
+            <div className="input-field col s12">
+                <input
+                   type="submit"
+                  value="Search"
+                  className=" waves-effect waves-light btn-large btn-block yellow accent-4"
+                  /> 
             </div>
         </form>
 
